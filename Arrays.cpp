2fe146cf1382c92426a65d414daa7ea347037b83
Aditya Pandey <adityapandey3082004@gmail.com>
2023-08-24 23:25:54 +0530

    array problems

--- Arrays.cpp
+++ Arrays.cpp
@@ -88,3 +88,86 @@ int unique(int arr[],int size){
 //     int a=unique(arr,9);
 //     cout<<a;
 // }
+
+
+// Intersection of __ARRAY_(sorted ones)
+
+vector<int> Intersection_array(int arr1[],int arr2[],int size1,int size2){
+    vector<int> ans;
+    for (int i=0;i<size1;i++){
+        for (int j=0;j<size2;j++){
+            if (arr1[i]<arr2[j]){
+                break;
+            }
+            if (arr1[i]=arr2[j]){
+                ans.push_back(arr1[i]);
+                arr2[j]=INT_MIN;
+                break;
+            }
+        }
+    }
+    return ans;
+}
+
+// optimised version
+
+vector<int> Intersection_array(int arr1[],int arr2[],int size1,int size2){
+    vector<int> ans;
+    int i=0;
+    int j=0;
+   while(i<size1 && j<size2){
+    while(arr1[i]<arr2[j]){
+        i++;
+    }
+    while(arr1[i]==arr2[j]){
+        ans.push_back(arr1[j]);
+        i++;
+        j++;
+    }
+    while(arr1[i]>arr2[j]){
+        j++;
+    }
+
+   }
+    return ans;
+}
+
+// Pair sum
+
+vector<vector<int>> pair_sum(int arr[],int size,int add){
+    vector<vector<int>> ans;
+     for (int i=0;i<size;i++){
+        for (int j=i+1;j<size;j++){
+            if (arr[i]+arr[j]==add){
+                 vector<int> temp;
+                 temp.push_back(min(arr[i],arr[j]));
+                 temp.push_back(max(arr[i],arr[j]));
+                 ans.push_back(temp);
+            }
+        }
+     }
+     sort(ans.begin(),ans.end())
+     return ans;
+}
+
+// triplet sum
+
+// sort 01
+
+void sort_01(int arr[],int size){
+    int start=0;
+    int end=size-1;
+    while(start<end){
+      while (arr[start]==0 && start<end){
+        start++;
+      }
+       while (arr[end]==1 && start<end){
+        end--;
+      }
+       if(start<end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+      }
+    }
+}
--- Leetcode_.cpp
+++ Leetcode_.cpp
@@ -174,3 +174,50 @@ public:
         return true;
     }
 };
+
+
+// 442.Duplicates in an array
+// Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears once or twice, return an array of all the integers that appears twice.
+
+// You must write an algorithm that runs in O(n) time and uses only constant extra space.
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        int i=0;
+        vector<int> ans;
+        while(i<nums.size()){
+            int count=1;
+            for (int j=i+1;j<nums.size();j++){
+                if (nums[i]==nums[j]){
+                    ans.push_back(nums[i]);
+                    i++;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+// another approach -optimized time comp:O(n) space comp:O(1)
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+       vector<int> ans;
+       for (int i=0;i<nums.size();i++){
+           int index=abs(nums[i])-1;
+           if (nums[index]<0){
+               ans.push_back(index+1);
+           }
+
+            nums[index]=nums[index]*(-1);
+       }
+       return ans;
+
+    }
+};
+
+
+
+
