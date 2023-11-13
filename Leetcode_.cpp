a60f2a10d84af9e0ff81a3f1ec9e1632a814ba4e
Aditya Pandey <adityapandey3082004@gmail.com>
2023-11-13 10:46:40 +0530

    Day 13.1

--- Leetcode_.cpp
+++ Leetcode_.cpp
@@ -1054,4 +1054,60 @@ public:
     }
 };
 
+//15.3Sum
 
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
+        if (nums.size()<3) return {};
+        sort(nums.begin(),nums.end());
+        for (int i=0;i+2<nums.size();i++){
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+           int l=i+1;
+           int r=nums.size()-1;
+           while(l<r){
+               int sum=nums[i]+nums[l]+nums[r];
+               if (sum==0){
+                    ans.push_back({nums[i], nums[l++], nums[r--]});
+                    while (l < r && nums[l] == nums[l - 1])
+                        ++l;
+                    while (l < r && nums[r] == nums[r + 1])
+                        --r;
+                }else if(sum<0){
+                    l++;
+                }else{
+                    r--;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+
+//78.subsets
+
+class Solution {
+private:
+    void solve(vector<int> nums,vector<int> output,int index,vector<vector<int>>& ans){
+        if(index>=nums.size()){
+            ans.push_back(output);
+            return;
+        }
+        solve(nums,output,index+1,ans);
+
+        int element=nums[index];
+        output.push_back(element);
+        solve(nums,output,index+1,ans);
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> output;
+        int index=0;
+        solve(nums,output,index,ans);
+        return ans;
+    }
+};
--- codingstudio.cpp
+++ codingstudio.cpp
@@ -574,3 +574,30 @@ bool checkPalindrome(long long N)
 	reverse(ch.begin(),ch.end());
 	return check(ch,0,ch.size()-1);
 }
+
+
+// Subsequences of String
+
+#include <bits/stdc++.h> 
+void solve(string str,string output,int index,vector<string>& ans){
+	if (index>=str.length()){
+		if (output.length()>0){
+			ans.push_back(output);
+		}
+		return;
+	}
+	solve(str,output,index+1,ans);
+
+	char element=str[index];
+	output.push_back(element);
+	solve(str,output,index+1,ans);
+}
+vector<string> subsequences(string str){
+	
+	vector<string> ans;
+	string output;
+	int index=0;
+	solve(str,output,index,ans);
+	return ans;
+	
+}
