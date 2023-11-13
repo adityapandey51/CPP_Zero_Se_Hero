0da69f57b0b2ff0fcae6e1d8796bea190dc379d9
Aditya Pandey <adityapandey3082004@gmail.com>
2023-11-13 13:22:56 +0530

    Day 13.3

--- Leetcode_.cpp
+++ Leetcode_.cpp
@@ -1139,3 +1139,30 @@ public:
         return ans;
     }
 };
+
+
+//46.Permutations
+//Given an array nums of distinct integers, 
+// return all the possible permutations. You can return the answer in any order.
+
+class Solution {
+private:
+    void solve(vector<int> nums,vector<vector<int>>& ans,int index){
+        if(index>=nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for (int i=index;i<nums.size();i++){
+            swap(nums[index],nums[i]);
+            solve(nums,ans,index+1);
+            swap(nums[index],nums[i]);
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int index=0;
+        solve(nums,ans,index);
+        return ans;
+    }
+};
