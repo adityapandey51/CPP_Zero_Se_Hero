07b39328fb12ab80ac91ab0f4cc7465139bc0221
Aditya Pandey <adityapandey3082004@gmail.com>
2023-11-13 19:31:39 +0530

    Day 13.4

--- recursion.cpp
+++ recursion.cpp
@@ -222,3 +222,77 @@ int main(){
     cout<<endl<<endl;
     cout<<"factorial of n is => "<<fact<<endl;
 }
+
+
+//Rat in a maze problem
+
+class Solution{
+    private:
+    bool isSafe(int newx,int newy,int n,vector<vector<int>> visited,vector<vector<int>> m ){
+        if ((newx<n && newx>=0) && (newy>=0 && newy<n) && (visited[newx][newy]==0) && (m[newx][newy]==1)){
+            return true;
+        }
+        return false;
+    }
+    void solve(vector<vector<int>> m,int x,int y,int n,vector<string> &ans,string path,vector<vector<int>> visited){
+        if (x==n-1 && y==n-1){
+            ans.push_back(path);
+            return;
+        }
+        visited[x][y]=1;
+        
+        int newx=x+1;
+        int newy=y;
+        if (isSafe(newx,newy,n,visited,m)){
+            path.push_back('D');
+            solve(m,newx,newy,n,ans,path,visited);
+            path.pop_back();
+        }
+        
+        newx=x;
+        newy=y-1;
+        if (isSafe(newx,newy,n,visited,m)){
+            path.push_back('L');
+            solve(m,newx,newy,n,ans,path,visited);
+            path.pop_back();
+        }
+        
+        newx=x-1;
+        newy=y;
+        if (isSafe(newx,newy,n,visited,m)){
+            path.push_back('U');
+            solve(m,newx,newy,n,ans,path,visited);
+            path.pop_back();
+        }
+        
+        newx=x;
+        newy=y+1;
+        if (isSafe(newx,newy,n,visited,m)){
+            path.push_back('R');
+            solve(m,newx,newy,n,ans,path,visited);
+            path.pop_back();
+        }
+        
+        
+        visited[x][y]=0;
+        
+    }
+    public:
+    vector<string> findPath(vector<vector<int>> &m, int n) {
+        vector<string> ans;
+        if(m[0][0]==0){
+            return ans;
+        }
+        vector<vector<int>> visited=m;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                visited[i][j]=0;
+            }
+        }
+        string path="";
+        int srcx=0;
+        int srcy=0;
+        solve(m,srcx,srcy,n,ans,path,visited);
+        return ans;
+    }
+};
