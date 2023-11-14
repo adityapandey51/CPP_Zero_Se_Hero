a1ba95c3b7760cdbbbfc1b0782d4468371c776c9
Aditya Pandey <adityapandey3082004@gmail.com>
2023-11-14 23:27:38 +0530

    day 14.1

--- Leetcode_.cpp
+++ Leetcode_.cpp
@@ -1166,3 +1166,21 @@ public:
         return ans;
     }
 };
+
+
+//26.Remove Duplicates from a sorted array
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size()==0) return 0;
+        int i=1;
+       while(i<nums.size()){
+            if(nums[i]!=nums[i-1]){
+                i++;
+            }else{
+                nums.erase(nums.begin()+i);
+            }
+        }
+        return nums.size();
+    }
+};
