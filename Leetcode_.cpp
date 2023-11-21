7a8c38ffc88c72fad2e044d46775c50dc94d4b66
Aditya Pandey <adityapandey3082004@gmail.com>
2023-11-21 23:00:59 +0530

    Day 21.2

--- Leetcode_.cpp
+++ Leetcode_.cpp
@@ -1381,3 +1381,40 @@ public:
         return dummy.next;
     }
 };
+
+//206.REVERSE LINKED LIST
+
+/*
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    void reverse(ListNode* &head,ListNode* curr,ListNode* prev){
+        if(curr==NULL){
+            head=prev;
+            return;
+        }
+
+        ListNode* forward=curr->next;
+        curr->next=prev;
+        reverse(head,forward,curr);
+    }
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* curr=head;
+        ListNode* prev=NULL;
+        reverse(head,curr,prev);
+        return head;
+    }
+};
--- codingstudio.cpp
+++ codingstudio.cpp
@@ -601,3 +601,45 @@ vector<string> subsequences(string str){
 	return ans;
 	
 }
+
+
+//REVERSE LL USING RECURSION
+
+#include <bits/stdc++.h>
+
+/****************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    template <typename T>
+    class LinkedListNode
+    {
+    public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+
+void reverse(LinkedListNode<int>* &head,LinkedListNode<int>* curr,LinkedListNode<int>* prev){
+    if(curr==NULL){
+        head=prev;
+        return;
+    }
+
+    LinkedListNode<int>* forward=curr->next;
+    curr->next=prev;
+    reverse(head,forward,curr);
+}
+LinkedListNode<int> *reverseLinkedList(LinkedListNode<int> *head) 
+{
+   LinkedListNode<int>* curr=head;
+   LinkedListNode<int>* prev=NULL;
+   reverse(head,curr,prev);
+   return head;
+}
